67bd15288b5dbc535d39999f884033f349711336
大中倫太郎 <ohnakarintaro@oonakarintarounoMacBook-Pro.local>
2021-01-23 16:30:05 +0900

    Fix: タイトル表示を即時関数に変更

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 // 即時関数にしてもいいかもね
-function displayTitle() {
+(function () {
+    console.log('即時関数のテスト');
     let titleId = document.getElementById('title');
     let h1Title = document.createElement('h1');
     let titleText = document.createTextNode('APIのテストページ');
     titleId.appendChild(h1Title);
     h1Title.appendChild(titleText);
-}
-displayTitle();
-
-(function () {
-    console.log('即時関数のテスト');
 }());
